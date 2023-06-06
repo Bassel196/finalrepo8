fd105305d47d84e569b456f0265722a2fa7795c4
Mostafa Ibrahim Mesbah <Mostafa2109107@miuegypt.edu.eg>
2023-06-06 10:48:05 +0300

    .

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require("../models/users");
+const Product = require("../models/product");
+const passport = require("passport");
+const Category = require("../models/category");
+const { sendOtp, getOtpForm } = require("../middleware/otp");
+
+module.exports = {
+  userRegister: (req, res, next) => {
+    if (req.body.password === req.body.confirmedPassword) {
+      User.register(
+        {
+          name: req.body.name,
+          email: req.body.email,
+        },
+        req.body.password,
+        async function (err, user) {
+          if (err) {
+            console.log(err);
+            req.flash("message", "User Already registered");
+            res.redirect("/register");
+          } else {
+            passport.authenticate("local")(req, res, function () {
+              // process.nextTick(async () => {
+              //   await sendOtp(req, res);
+              // });
+              res.redirect("/");
+            });
+          }
+        }
+      );
+    } else {
+      req.flash("message", "password doesn't match");
+      res.redirect("/register");
+    }
+  },
+};
