ce55de4006b7a64cd756a2af70bc39f445984eef
Mohab Saber <125674416+MohabSaber@users.noreply.github.com>
2023-06-06 17:36:37 +0300

    Update wishlistController.js

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -14,3 +14,35 @@ module.exports = {
             findWishlist: findWishlist
         })
     },
+    addToWishlist: async (req, res, next) => {
+        try {
+            const userId = req.user.id
+            const name = req.body.name
+            const productId = req.params.id
+            const myWishlist = await Wishlist.findOne({ userId: userId })
+
+            if (myWishlist) {
+                const ItemIndex = myWishlist.myList.findIndex(product => product.productId == productId)
+                if (ItemIndex > -1) {
+                    myWishlist.myList.splice(ItemIndex, 1)
+                    await myWishlist.save()
+                    return res.status(204).json({ message: "removed from wishlist" })
+                } else {
+                    myWishlist.myList.push({ productId, name })
+                    await myWishlist.save()
+                    return res.status(201).json({ message: "added to wishlist" })
+                }
+            } else {
+                await Wishlist.create({
+                    userId: userId,
+                    myList: [{ productId, name }]
+                })
+                return res.status(201).json({ message: "added to wishlist" })
+            }
+
+
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({ err })
+        }
+    },
